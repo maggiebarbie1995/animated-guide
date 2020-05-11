af68d81111279feda0a5c92ae75713e83ec29117
maggiebarbie1995 <maggylangakush@gmail.com>
2020-05-11 13:29:00 +0300

    added Jquery function for collecting user inputs from the form

--- Delanistudio/script.js/script.js
+++ Delanistudio/script.js/script.js
@@ -5,4 +5,11 @@ let comment = function(name, email) {
       alert(name + "We have received your message.Thanks for reaching out to us.");
     }
   }
-  
+  $(document).ready(function(){
+    $("form").submit(function(event){
+      event.preventDefault();
+      var userName = $("#name").val();
+      var email = $("#email").val();
+      comment(userName, email);
+    });
+  });
