3b9da5f33fd3d1c95fadd63f358a008d63c71248
maggiebarbie1995 <maggylangakush@gmail.com>
2020-05-11 13:20:52 +0300

    added function if and alert

--- Delanistudio/script.js/script.js
+++ Delanistudio/script.js/script.js
@@ -1,1 +1,8 @@
-
+let comment = function(name, email) {
+    if (!name || !email) {
+      alert("Please enter your name.");
+    }else {
+      alert(name + "We have received your message.Thanks for reaching out to us.");
+    }
+  }
+  
